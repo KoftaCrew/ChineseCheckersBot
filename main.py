38274cfaeadb63cde8e99481f59775744adc4b92
Kofta <mostafamorsy22@gmail.com>
2022-06-02 23:13:26 +0200

    Heurstic Funciton

--- main.py
+++ main.py
@@ -5,6 +5,30 @@ import tkinter as tk
 from checker_frame import CheckerFrame
 from game_state import GameState
 
+def getHeuristic(gameState, player: int):
+    ret = 0
+    if player == 0:       # white
+        return ret
+    elif player == 1:     # green player
+        for p in gameState.map:
+            if gameState.map(p) == player:
+                mx = 0
+                for y in range(4):
+                    for x in range(4 - y):
+                        dist = abs(p[0] - x) + abs(p[1] - (y + 9))
+                        mx = max(mx, dist)
+                ret+= mx
+    else:               # red player
+        for p in gameState.map:
+            if gameState.map(p) == player:
+                mx = 0
+                for y in range(4):
+                    for x in range(y + 1):
+                        dist = abs(p[0] - (x - y + 8)) + abs(p[1] - (y - 4))
+                        mx = max(mx, dist)
+                ret+= mx
+    return ret
+
 def ThreadFunction(checkers: CheckerFrame):
     gameState = checkers.canvas.gameState
     moves = gameState.getAvailableMoves(2)
