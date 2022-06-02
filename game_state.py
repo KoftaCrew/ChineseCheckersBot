6e673a0603a4dbe8ac0ad5b48701a9239523391b
Kareem Morsy <kreem.morsy@hotmail.com>
2022-06-03 01:38:06 +0200

    Heuristic refactor

--- game_state.py
+++ game_state.py
@@ -109,3 +109,29 @@ class GameState:
         result.map[next] = temp
         
         return result
+
+    def getHeuristic(self, player: int) -> int:
+        ret = 0
+        if player == 0:       # white
+            return ret
+        elif player == 1:     # green player
+            for cords, p in self.map.items():
+                if p == player:
+                    mx = 0
+                    for y in range(4):
+                        for x in range(4 - y):
+                            if self.map.get((x, y + 9)) == 0:
+                                dist = abs(p[0] - x) + abs(p[1] - (y + 9))
+                                mx = max(mx, dist)
+                    ret+= mx
+        else:               # red player
+            for cords, p in self.map.items():
+                if p == player:
+                    mx = 0
+                    for y in range(4):
+                        for x in range(y + 1):
+                            if self.map.get((x - y + 8, y - 4)):
+                                dist = abs(p[0] - (x - y + 8)) + abs(p[1] - (y - 4))
+                                mx = max(mx, dist)
+                    ret+= mx
+        return ret
--- main.py
+++ main.py
@@ -5,35 +5,9 @@ import tkinter as tk
 from checker_frame import CheckerFrame
 from game_state import GameState
 
-def getHeuristic(gameState, player: int):
-    ret = 0
-    if player == 0:       # white
-        return ret
-    elif player == 1:     # green player
-        for p in gameState.map:
-            if gameState.map(p) == player:
-                mx = 0
-                for y in range(4):
-                    for x in range(4 - y):
-                        if gameState.map(x, y + 9) == 0:
-                            dist = abs(p[0] - x) + abs(p[1] - (y + 9))
-                            mx = max(mx, dist)
-                ret+= mx
-    else:               # red player
-        for p in gameState.map:
-            if gameState.map(p) == player:
-                mx = 0
-                for y in range(4):
-                    for x in range(y + 1):
-                        if gameState.map(x - y + 8, y - 4):
-                            dist = abs(p[0] - (x - y + 8)) + abs(p[1] - (y - 4))
-                            mx = max(mx, dist)
-                ret+= mx
-    return ret
-
 def ThreadFunction(checkers: CheckerFrame):
     gameState = checkers.canvas.gameState
-    moves = gameState.getAvailableMoves(2)
+    moves = gameState.getAvailableMoves(1)
     for m in moves:
         sleep(1)
         global stopThread
@@ -41,6 +15,7 @@ def ThreadFunction(checkers: CheckerFrame):
             return
         
         checkers.canvas.gameState = m
+        print(m.getHeuristic(1))
         checkers.canvas.updateCircles()
 
 if __name__ == "__main__":
